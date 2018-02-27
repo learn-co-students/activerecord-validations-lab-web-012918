d0c176e4711a3a2b956d398eb8881b97bdaf57bc
github username <github email address>
2018-02-27 14:29:22 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  # validates :check_title
+  #
+  # BAIT = [
+  #   /Won't Believe/i,
+  #   /Secret/i,
+  #   /Top [0-9]*/i,
+  #   /Guess/i
+  # ]
+  #
+  # def check_title
+  #   if BAIT.none? {|pattern| pattern.match title}
+  #     errors.add(:title, "must be clickbait")
+  #   end
+  # end
 end
