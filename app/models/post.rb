fbbc9c173bbf9cfd1dbf93797864f6d4307b58c9
alin684 <al684@cornell.edu>
2018-02-27 10:59:59 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :exciting_title
+
+  def exciting_title
+    if title != nil
+      if %w(Won't/ Believe Secret Top Guess).any? { |s| title.include? s }
+      else
+        errors.add(:title, "Not Exciting Enough")
+      end
+    end
+  end
 end
